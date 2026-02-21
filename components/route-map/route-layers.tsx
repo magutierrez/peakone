6c3c4414637cb494c7ba9c4834d56fc0fae02b10
Miguel Angel <miguelangel.gutierrez@deporvillage.com>
2026-02-21 17:59:05 +0100

    feat: redesign

--- components/route-map/route-layers.tsx
+++ components/route-map/route-layers.tsx
@@ -42,26 +42,6 @@ export function RouteLayers({
             }}
             layout={{ 'line-cap': 'round', 'line-join': 'round' }}
           />
-          <Layer
-            id="route-direction-arrows"
-            type="symbol"
-            layout={{
-              'symbol-placement': 'line',
-              'symbol-spacing': 80, // More frequent arrows
-              'text-field': '>',
-              'text-size': ['interpolate', ['linear'], ['zoom'], 10, 12, 18, 24],
-              'text-keep-upright': false,
-              'text-allow-overlap': true,
-              'text-rotate': 0, // In line-placement, 0 is along the line
-              'text-font': ['Open Sans Regular', 'Arial Unicode MS Regular'],
-            }}
-            paint={{
-              'text-color': mapType === 'standard' ? '#065f46' : '#ffffff',
-              'text-opacity': activeFilter || selectedRange ? 0.4 : 1,
-              'text-halo-color': mapType === 'standard' ? '#ffffff' : '#000000',
-              'text-halo-width': 2,
-            }}
-          />
         </Source>
       )}
 
@@ -105,6 +85,31 @@ export function RouteLayers({
             id="range-line"
             type="line"
             paint={{ 'line-color': '#007aff', 'line-width': 5, 'line-opacity': 1 }}
+          />
+        </Source>
+      )}
+
+      {routeData && (
+        <Source id="route-arrows-source" type="geojson" data={routeData}>
+          <Layer
+            id="route-direction-arrows"
+            type="symbol"
+            layout={{
+              'symbol-placement': 'line',
+              'symbol-spacing': 80, // More frequent arrows
+              'text-field': '>',
+              'text-size': ['interpolate', ['linear'], ['zoom'], 10, 12, 18, 24],
+              'text-keep-upright': false,
+              'text-allow-overlap': true,
+              'text-rotate': 0, // In line-placement, 0 is along the line
+              'text-font': ['Open Sans Regular', 'Arial Unicode MS Regular'],
+            }}
+            paint={{
+              'text-color': mapType === 'standard' ? '#065f46' : '#ffffff',
+              'text-opacity': activeFilter || selectedRange ? 0.4 : 1,
+              'text-halo-color': mapType === 'standard' ? '#ffffff' : '#000000',
+              'text-halo-width': 2,
+            }}
           />
         </Source>
       )}
