f9c28a725eaa5001bb1a2b95e4c0f95ca2439f07
Miguel Angel <miguelangel.gutierrez@deporvillage.com>
2026-02-22 20:04:37 +0100

    feat: zudstad

--- components/route-map/use-map-terrain.ts
+++ components/route-map/use-map-terrain.ts
@@ -8,19 +8,19 @@ export function useMapTerrain(mapRef: RefObject<MapRef | null>, mapStyle: any) {
     const map = mapRef.current?.getMap();
     if (!map || !map.isStyleLoaded()) return;
 
-    if (!map.getSource('open-terrain')) {
-      map.addSource('open-terrain', {
-        type: 'raster-dem',
-        tiles: ['https://s3.amazonaws.com/elevation-tiles-prod/terrarium/{z}/{x}/{y}.png'],
-        encoding: 'terrarium',
-        tileSize: 256,
-        maxzoom: 14,
-      });
-    }
-
-    if (!map.getTerrain()) {
-      map.setTerrain({ source: 'open-terrain', exaggeration: 1.5 });
-    }
+    // if (!map.getSource('open-terrain')) {
+    //   map.addSource('open-terrain', {
+    //     type: 'raster-dem',
+    //     tiles: ['https://s3.amazonaws.com/elevation-tiles-prod/terrarium/{z}/{x}/{y}.png'],
+    //     encoding: 'terrarium',
+    //     tileSize: 256,
+    //     maxzoom: 14,
+    //   });
+    // }
+    //
+    // if (!map.getTerrain()) {
+    //   map.setTerrain({ source: 'open-terrain', exaggeration: 1.5 });
+    // }
   }, [mapRef]);
 
   useEffect(() => {
